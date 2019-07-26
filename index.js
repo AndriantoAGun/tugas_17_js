1e182bc1fc7e2f62c2de23002c51ca7faf9e86ee
AndriantoAGun <andrianto22491@gmail.com>
2019-07-26 08:21:34 +0700

    Tugas 17 JavaScript
    tugas Niomic tentang JavaScript meliputi modifikasi String
    a. file index : toLowerCase dan toUpperCase
    b. file lndex : charCodeAt (kode ASCII dari String)

---
+++ index.js
@@ -0,0 +1,13 @@
+var Sentence1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+var Sentence2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+var Sentence1low = Sentence1.toLowerCase();
+var Sentence1upp = Sentence1.toUpperCase();
+
+var Sentence2low = Sentence2.toLowerCase();
+var Sentence2upp = Sentence2.toUpperCase();
+
+console.log(Sentence1low);
+console.log(Sentence1upp);
+console.log(Sentence2low);
+console.log(Sentence2upp);
